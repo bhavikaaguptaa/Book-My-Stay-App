17982a9bffa9e32792157e6ec3958bcb71296ff4
bhavikaaguptaa <bg2291@srmist.edu.in>
2026-04-17 20:34:02 +0530

    Add files via upload
    Use Case 1: Application Entry & Welcome Message

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,19 @@
+/**
+ * Book My Stay App
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * @author Bhavika
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("==================================");
+
+    }
+}
